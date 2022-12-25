23b8e30ed6632e23d35541955dadb877882ea6a0
Shakila Bithi <shakilahossain036@gmail.com>
2022-12-24 22:49:26 -0500

    first commit

---
+++ src/mobile/hello.java
@@ -0,0 +1,34 @@
+package mobile;
+
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("i have mobile");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
